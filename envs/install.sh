b1c1a119674956655753f3c91e9683ade9797f3d
mylena-s <57195250+mylena-s@users.noreply.github.com>
2025-05-15 08:38:52 -0300

    update

---
+++ envs/install.sh
@@ -0,0 +1,3 @@
+wget https://github.com/PacificBiosciences/pb-CpG-tools/releases/download/v2.3.2/pb-CpG-tools-v2.3.2-x86_64-unknown-linux-gnu.tar.gz
+tar -xzf pb-CpG-tools-v2.3.2-x86_64-unknown-linux-gnu.tar.gz
+
