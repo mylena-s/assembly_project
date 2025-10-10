6e49ab264922e5e17e7049b44e07756cf2ec0a73
mylena-s <57195250+mylena-s@users.noreply.github.com>
2025-10-10 08:38:03 -0300

    Delete scripts directory

--- scripts/coverage.sh
+++
@@ -1,3 +0,0 @@
-bedtools coverage -a CBFH00195_synteny.windows -b CBFH00251_sniffles_clean.bed > CBFH00251_coverage.bed
-bedtools coverage -a CBFH00195_synteny.windows -b CBFH00195_sniffles_clean.bed > CBFH00195_coverage.bed
-
